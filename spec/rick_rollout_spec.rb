5a9dd2b38c1494669c4e579b8d92d1f0411857b6
poulping <arnaud@godynamo.com>
2017-01-11 11:18:10 -0500

    adds tests

--- spec/rick_rollout_spec.rb
+++ spec/rick_rollout_spec.rb
@@ -1,11 +1,38 @@
 require "spec_helper"
 
-describe RickRollout do
-  it "has a version number" do
-    expect(RickRollout::VERSION).not_to be nil
+RSpec.describe RickRollout::Feature.new({}) do
+
+  before(:all) do
+    RickRollout.configure do |config|
+      rollout = Rollout.new(Redis.new(port: 16379))
+
+      config.rollout = rollout
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.active? with rollout.active? returning false' do
+    it 'returns false if feature flags are enabled' do
+      allow(ENV).to receive(:fetch) { '1' }
+      expect(subject.active?({})).to be_falsey
+    end
+
+    it 'returns true if feature flags are disabled' do
+      allow(ENV).to receive(:fetch) { '0' }
+      expect(subject.active?({})).to be_truthy
+    end
+  end
+
+  describe '.active? with rollout.active? returning true' do
+    it 'returns true if rollout is active and feature flags are disabled' do
+      allow(RickRollout.configuration.rollout).to receive(:active?) { true }
+      allow(ENV).to receive(:fetch) { '0' }
+      expect(subject.active?({})).to be_truthy
+    end
+
+    it 'returns true if rollout is active feature flags are enabled' do
+      allow(RickRollout.configuration.rollout).to receive(:active?) { true }
+      allow(ENV).to receive(:fetch) { '1' }
+      expect(subject.active?({})).to be_truthy
+    end
   end
 end
